7225cfbbed8d5556f26c2794d0ba9a969adcce6e
AlexGooday <alexey.mostovoy.w@gmail.com>
2024-04-04 17:57:31 +0300

    Wrapped crash with downloadable fonts

--- avatar-view/src/main/java/com/goodayapps/widget/utils/TypedArray.kt
+++ avatar-view/src/main/java/com/goodayapps/widget/utils/TypedArray.kt
@@ -9,21 +9,24 @@ import androidx.core.content.res.ResourcesCompat
 
 @ColorInt
 fun TypedArray.getColorOrNull(@StyleableRes index: Int): Int? {
-	val color = getColor(index, Integer.MAX_VALUE)
+    val color = getColor(index, Integer.MAX_VALUE)
 
-	return if (color == Integer.MAX_VALUE) {
-		null
-	} else {
-		color
-	}
+    return if (color == Integer.MAX_VALUE) {
+        null
+    } else {
+        color
+    }
 }
 
 fun TypedArray.getTypefaceOrNull(context: Context, @StyleableRes index: Int): Typeface? {
-	val resId = getResourceId(index, 0)
-
-	return if (resId != 0) {
-		ResourcesCompat.getFont(context, resId)
-	}else{
-		null
-	}
+    return try {
+        val resId = getResourceId(index, 0)
+        if (resId != 0) {
+            ResourcesCompat.getFont(context, resId)
+        } else {
+            null
+        }
+    } catch (e: Exception) {
+        null
+    }
 }
